886a69a70513a457413b9977bdd09a45220e6666
szymon <szymon@DESKTOP-T1DO8P0>
2026-05-24 13:08:10 +0200

    Dodanie usługi Authentication do konfiguracji Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// 1. Dodajemy import modułu autoryzacji
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,4 +15,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// 2. Inicjalizujemy usługę Authentication i przypisujemy ją do zmiennej
+const auth = getAuth(app);
+
+// 3. Eksportujemy auth, aby móc z niego korzystać w komponentach logowania i rejestracji
+export { auth };
 export default app;
